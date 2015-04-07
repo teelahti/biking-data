b3e24b6bf79ab9bb48d58ac6822ed560d3b43747
Tero Teelahti <tero@teelahti.fi>
2015-04-07 23:31:47 +0300

    First graphs visible

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 (function() {
   "use strict";
-  /* global d3, MG*/
+  /* global d3, nv */
 
   function Dictionary() { }
 
@@ -25,52 +25,93 @@
     }, this);
   };
 
-  function createVisualization(element, title, data, min_y, baselines) {
-    // TODO: Consider changing to C3 http://c3js.org/
-    // or NVD3.js https://github.com/liquidpele/nvd3
+  Dictionary.prototype.toD3Array = function() {
+    return Object.getOwnPropertyNames(this).map(function(person) {
+      return {
+        key: person,
+        values: this[person]
+          .map(function(elem) { return [elem.date, elem.value]; })
+        };
+    }, this);
+  };
 
-    // TODO: Install with bower, just commit bower_components
+  // TODO: Add link graphs on rollover (follow all timeline graphs simultaneously)
+  // TODO: Add baselines: 82 kg läski, 40 cm norsupohje
+  function sum(a, b) {return a + b;}
 
-    // TODO: Add pie chart of the km distribution
+  function visualizeTotalKMDistribution(element, data) {
 
-    MG.data_graphic({
-        title: title,
-        data: data.toArray(),
+    // Calculate totals
+    var kmdata = Object.getOwnPropertyNames(data).map(function(person) {
+      return {
+        person: person,
+        kms: this[person]
+          .map(function(elem) { return elem.value; })
+          .reduce(sum)
+      };
+    }, data);
 
-        // Resize dynamically
-        full_width: true,
-        full_height: true,
-        top: 20,
+    var totalKms = kmdata
+        .map(function(elem) { return elem.kms })
+        .reduce(sum);
 
-        target: element,
-        linked: true,
-        aggregate_rollover: true,
-        decimals: 2,
-        xax_format: d3.time.format("%-d.%-m"),
-        show_secondary_x_label: false,
+    var height = 350;
+    var width = 350;
 
-        legend: Object.getOwnPropertyNames(data),
+    nv.addGraph(function() {
+       var chart = nv.models.pieChart()
+           .donut(true)
+           .title(totalKms + " km")
+           .showLabels(false)
+           .x(function(d) { return d.person })
+           .y(function(d) { return d.kms })
+           .valueFormat(function (d) { return d + " km"; })
+           .width(width)
+           .height(height)
+           .padAngle(.08)
+           .cornerRadius(5)
+           .id('kmdistribution'); // allow custom CSS for this one svg
 
-        // TODO: Make legend fixed on top
-        legend_target: "#legend",
+       // TODO: Transition not working at the moment
+       d3.select(element)
+           .datum(kmdata)
+           .transition().duration(1200)
+           .attr('width', width)
+           .attr('height', height)
+           .call(chart);
 
-        // Tooltips require jquery, disable them
-        show_tooltips: false,
+       nv.utils.windowResize(chart.update);
 
-        animate_on_load: true,
-
-        min_y: min_y,
-
-        baselines: baselines
+       return chart;
     });
-
   }
 
-  // Attach cumulative graph toggle
-  var cumulativeToggle = document.getElementById("cumulative-toggle");
-  cumulativeToggle.addEventListener("change", function(e) {
-      document.body.classList.toggle("show-cumulative");
-  });
+  function visualizeTimeLine(element, data) {
+    nv.addGraph(function() {
+        var chart = nv.models.cumulativeLineChart()
+            .useInteractiveGuideline(true)
+            .x(function(d) { return d[0] })
+            .y(function(d) { return d[1] })
+            // .color(d3.scale.category10().range())
+            // .average(function(d) { return d.mean/100; })
+            .duration(300)
+            .clipVoronoi(false);
+
+        chart.xAxis.tickFormat(function(d) {
+            return d3.time.format('%-d.%-m')(new Date(d))
+        });
+
+        //chart.yAxis.tickFormat(d3.format(',.1 km'));
+
+        d3.select(element)
+            .datum(data.toD3Array())
+            .call(chart);
+
+        nv.utils.windowResize(chart.update);
+
+        return chart;
+    });
+  }
 
   // Get data from server
   d3.json("data/data.json", function(data) {
@@ -100,13 +141,13 @@
         });
       });
 
-      createVisualization("#vis-km", "Kilometrit", kms);
+      visualizeTotalKMDistribution("#vis-total-distribution", kms);
 
-      // BUG: Rendering the cumulative graph on hidden element crashes metricsgraphics.js
-      // createVisualization("#vis-km-cumulative", "Kilometrit", kms_cumulative);
-      createVisualization("#vis-weight", "Paino", weights, 75, [{value: 82, label:"Läski"}]);
-      createVisualization("#vis-waist", "Vyötärö", waists, 85);
-      createVisualization("#vis-thigh", "Reisi", thighs, 45);
-      createVisualization("#vis-calf", "Pohje", calfs, 34, [{value: 40, label:"Norsupohje"}]);
+      visualizeTimeLine("#vis-km", kms);
+
+      // createVisualization("#vis-weight", "Paino", weights, 75, [{value: 82, label:"Läski"}]);
+      // createVisualization("#vis-waist", "Vyötärö", waists, 85);
+      // createVisualization("#vis-thigh", "Reisi", thighs, 45);
+      // createVisualization("#vis-calf", "Pohje", calfs, 34, [{value: 40, label:"Norsupohje"}]);
   });
 }());
