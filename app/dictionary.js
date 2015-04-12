c399a1ccfea6a077498e32458e5ec1debbec15e5
Tero Teelahti <tero@teelahti.fi>
2015-04-12 22:16:07 +0300

    Extract Dictionary to separate file

---
+++ app/dictionary.js
@@ -0,0 +1,34 @@
+// TODO: Consider using module format and loader, e.g. https://github.com/ModuleLoader/es6-module-loader
+
+function Dictionary() { }
+
+Dictionary.prototype.push = function(person, date, value) {
+  if(!this.hasOwnProperty(person)) {
+    this[person] = [];
+  }
+
+  this[person].push({ "date": date, "value": value});
+};
+
+Dictionary.prototype.pushCumulative = function(person, date, value) {
+  var p = this[person],
+      prev = (p && p.length) ? p[p.length - 1].value : 0;
+
+  this.push(person, date, prev + value);
+};
+
+Dictionary.prototype.toArray = function() {
+  return Object.getOwnPropertyNames(this).map(function(person) {
+    return this[person];
+  }, this);
+};
+
+Dictionary.prototype.toD3Array = function() {
+  return Object.getOwnPropertyNames(this).map(function(person) {
+    return {
+      key: person,
+      values: this[person]
+        .map(function(elem) { return [elem.date, elem.value]; })
+      };
+  }, this);
+};
--- app/main.js
+++ app/main.js
@@ -2,39 +2,6 @@
   "use strict";
   /* global d3, nv */
 
-  function Dictionary() { }
-
-  Dictionary.prototype.push = function(person, date, value) {
-    if(!this.hasOwnProperty(person)) {
-      this[person] = [];
-    }
-
-    this[person].push({ "date": date, "value": value});
-  };
-
-  Dictionary.prototype.pushCumulative = function(person, date, value) {
-    var p = this[person],
-        prev = (p && p.length) ? p[p.length - 1].value : 0;
-
-    this.push(person, date, prev + value);
-  };
-
-  Dictionary.prototype.toArray = function() {
-    return Object.getOwnPropertyNames(this).map(function(person) {
-      return this[person];
-    }, this);
-  };
-
-  Dictionary.prototype.toD3Array = function() {
-    return Object.getOwnPropertyNames(this).map(function(person) {
-      return {
-        key: person,
-        values: this[person]
-          .map(function(elem) { return [elem.date, elem.value]; })
-        };
-    }, this);
-  };
-
   // TODO: Add link graphs on rollover (follow all timeline graphs simultaneously)
   // TODO: Add baselines: 82 kg läski, 40 cm norsupohje
   function sum(a, b) {return a + b;}
